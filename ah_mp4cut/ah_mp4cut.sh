4c97ccdea96ccf9acdf44a91b544c894cc11ea40
Andy Hafner <a.hafner@kmu-net.ch>
2016-09-28 13:29:11 +0200

    Version 1.01, internally converted to function blocks

--- ah_mp4cut/ah_mp4cut.sh
+++ ah_mp4cut/ah_mp4cut.sh
@@ -1,28 +1,41 @@
 #!/bin/bash
 
-# Version 1.0
+# Version 1.01, internally converted to function blocks
 
-input_file=$1
-echo "Input_file: $input_file"
+init() {
 
-destination_file="/mnt/lv_md0/$input_file"
-echo "Destination_file: $destination_file"
+	input_file=$1
+	echo "Input_file: $input_file"
+
+	destination_file="/mnt/lv_md0/$input_file"
+	echo "Destination_file: $destination_file"
+
+}
 
 convert_hms2seconds() {
     seconds="$(echo $hms | awk -F: '{ print ($1 * 3600) + ($2 * 60) + $3 }')"
 }
 
-hms=$2
-convert_hms2seconds
-start=$seconds
-echo "Start of extraction at: $2"
+main() {
 
-hms=$3
-convert_hms2seconds
-end=$seconds
-echo "End of extraction at: $3"
+	hms=$1
+	convert_hms2seconds
+	start=$seconds
+	echo $start
+	echo "Start of extraction at: $1"
 
-duration="$(expr $end - $start)"
-echo "Lenght of extraction in seconds: $duration"
+	hms=$2
+	convert_hms2seconds
+	end=$seconds
+	echo $end
+	echo "End of extraction at: $2"
 
-avconv -ss $start -i $input_file -acodec copy -vcodec copy -t $duration $destination_file
+	duration="$(expr $end - $start)"
+	echo "Lenght of extraction in seconds: $duration"
+
+	avconv -ss $start -i $input_file -acodec copy -vcodec copy -t $duration $destination_file
+
+}
+
+init $1
+main $2 $3
